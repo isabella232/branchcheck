acbe5ed6a1deff950d9f074bcf918a5b57a965b9
Mark Petrovic <mark.petrovic@xoom.com>
2014-10-14 17:58:19 -0700

    first pass

--- branchcheck.go
+++ branchcheck.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+)
+
+type POM struct {
+	XMLName xml.Name `xml:"project"`
+	Version string   `xml:"version"`
+}
 
 func main() {
-	fmt.Println("hello")
+	branchName, err := CurrentBranchName()
+	if err != nil {
+		fmt.Printf("Cannot determine current branch name\n", err)
+		return
+	}
+
+	poms, err := FindPoms()
+	if err != nil {
+		fmt.Printf("Cannot determine current branch name\n", err)
+		return
+	}
+
+	for _, f := range poms {
+		data, err := ioutil.ReadFile(f)
+		if err != nil {
+			fmt.Printf("Error reading %s: %v\n", f, err)
+			return
+		}
+		var pom POM
+		reader := bytes.NewBuffer(data)
+		if err := xml.NewDecoder(reader).Decode(&pom); err != nil {
+			fmt.Printf("error parsing pom.xml: %v\n", err)
+		}
+		if branchName == "develop" && !IsValidDevelopVersion(pom.Version) {
+			fmt.Printf("invalid develop branch version %s in %s\n", pom.Version, f)
+			os.Exit(-1)
+		}
+	}
+}
+
+func CurrentBranchName() (string, error) {
+	command := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	if data, err := command.Output(); err != nil {
+		return "", err
+	} else {
+		return string(data[:len(data)-1]), nil
+	}
+}
+
+func IsValidDevelopVersion(version string) bool {
+	match, _ := regexp.MatchString("[1-9]+\\.[0-9]+-SNAPSHOT", version)
+	return match
+}
+
+func FindPoms() ([]string, error) {
+	files := make([]string, 0)
+	err := filepath.Walk(".", filepath.WalkFunc(func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == "pom.xml" {
+			files = append(files, path)
+		}
+		return nil
+	}))
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
 }
---
+++ develop_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestDevelopVersion(t *testing.T) {
+	versions := []string{"1.0-SNAPSHOT", "2.14-SNAPSHOT"}
+	for _, version := range versions {
+		b := IsValidDevelopVersion(version)
+		if !b {
+			t.Fatalf("IsValidDevelopVersion(%s) expecting true", version)
+		}
+	}
+}
+
+func TestInvalidDevelopVersion(t *testing.T) {
+	versions := []string{"1.0-us_feature-SNAPSHOT", "2.14-bug_77-SNAPSHOT"}
+	for _, version := range versions {
+		b := IsValidDevelopVersion(version)
+		if b {
+			t.Fatalf("IsValidDevelopVersion(%s) expecting true", version)
+		}
+	}
+}
